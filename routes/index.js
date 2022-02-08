2117b62fa399fafa0b22ea03f4fe88ad68f7cd0b
technotik2 <peymanth27@gmail.com>
2022-02-07 19:24:27 -0500

    final touchups(submission)

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
+router.get('/', function (req, res) {
+  res.render('Home', { title: 'Home' });
+});
 /* GET home page. */
 router.get('/home', function (req, res) {
   res.render('Home', { title: 'Home' });
 });
-
 /* GET About page. */
 router.get('/about', function(req, res, next) {
   res.render('About_me', { title: 'About' });
 });
-
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
   res.render('Services', { title: 'Services' });
@@ -17,11 +18,9 @@ router.get('/services', function(req, res, next) {
 /* GET Products page. */
 router.get('/projects', function(req, res, next) {
   res.render('Projects', { title: 'Projects' });
-  
 });
 /* GET Contact page. */
 router.get('/contact', function(req, res) {
   res.render('Contact', { title: 'Contact' });
-  
 });
 module.exports = router;
