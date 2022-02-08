116a8b5770f81fda83532d3c58d44903dc79e971
technotik2 <peymanth27@gmail.com>
2022-02-07 22:21:24 -0500

    added Procfile file

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ var http = require('http');
 
  var port;
  app.set('port', port);
-// app.listen(process.env.PORT || 3000, function(){
-//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-// });
+app.listen(process.env.PORT || 3000, function(){
+ console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+ });
 /**
  * Create HTTP server.
  */
